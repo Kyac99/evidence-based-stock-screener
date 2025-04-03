ce02b784881df085aa8e8c0326d59beb12102c5c
Pêgdwendé Yacouba KONSEIGA <116833043+Kyac99@users.noreply.github.com>
2025-04-03 09:11:19 +0200

    Mise à jour du __init__.py pour la visualisation

--- src/visualization/__init__.py
+++ src/visualization/__init__.py
@@ -1,1 +1,7 @@
-# Data visualization tools
+"""
+Package de visualisation pour le Evidence-Based Stock Screener.
+"""
+
+from src.visualization.chart_generator import ChartGenerator
+
+__all__ = ['ChartGenerator']
